00ec68217f6144eb61456dab81c3ae046e7f77cd
Amanda Lira <amandalira.adm@gmail.com>
2020-11-29 15:45:22 +0000

    Minha Pagina

--- avaliacao/app/core/Controller.php
+++ avaliacao/app/core/Controller.php
@@ -4,13 +4,13 @@ class Controller {
 
     // Create a new instance of a model
     protected function model($model) {
-        require_once '../app/models/' . $model . '.html';
+        require_once '../app/models/' . $model . '.php';
         return new $model();
     }
 
     // Load data to a specific view
     protected function view($view, $data = []) {
-        require_once '../app/views/' . $view . '.html';
+        require_once '../app/views/' . $view . '.php';
     }
 }
 
